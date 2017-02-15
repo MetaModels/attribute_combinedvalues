cef9ff5586bbbd32e9b23e43246f5a361023b101
Mini Model <minimodel@metamodel.me>
2017-02-15 08:14:58 +0100

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_attribute.php
+++ contao/languages/de/tl_metamodel_attribute.php
@@ -8,16 +8,16 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T04:12:56+02:00
+ * last-updated: 2017-02-15T04:15:25+01:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['0']    = 'Felder';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['1']    = 'Bitte wählen Sie eines oder mehrere Attribute zum kombinieren aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['1']    = 'Bitte wählen Sie eines oder mehrere Attribute zum Kombinieren aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['0']    = 'Format';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['1']    = 'Benutzen Sie "%s" oder einen anderen Platzhalter um formatierten text auszugeben. Weitere Informationen finden Sie unter  <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a>. ';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['1']    = 'Verwenden Sie "%s" oder einen anderen Platzhalter zur Erstellung des kombinierten Textes. Mehr zu den Platzhaltern unter <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a>.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']               = 'Attribute';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['0']     = 'Aktualisierung erzwingen';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['1']     = 'Auswählen, um eine Neuerstellung der kombinierten Werte erzwingen, wenn sich eines der abhängigen Felder ändert.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['1']     = 'Auswählen, um eine Neuerstellung der kombinierten Werte zu erzwingen, wenn sich eines der abhängigen Felder ändert.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['attributes']   = 'Attribute';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['id']           = 'ID';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['meta']         = 'Metafelder';
