478626c229bab83da4586eee90e03a27bcfd1f86
Ingolf Steinhardt <info@e-spin.de>
2018-03-26 08:19:07 +0200

    Hotfix move contents of database.sql to dca files

--- contao/dca/tl_metamodel_attribute.php
+++ contao/dca/tl_metamodel_attribute.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_combinedvalues.
  *
- * (c) 2012-2016 The MetaModels team.
+ * (c) 2012-2018 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,8 @@
  * @author     Andreas Isaak <andy.jared@googlemail.com>
  * @author     David Greminger <david.greminger@1up.io>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
- * @copyright  2012-2016 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2018 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_combinedvalues/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -33,25 +34,26 @@ $GLOBALS['TL_DCA']['tl_metamodel_attribute']['metapalettes']['combinedvalues ext
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['combinedvalues_fields'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields'],
-    'exclude'                 => true,
-    'inputType'               => 'multiColumnWizard',
-    'eval'                    => array
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields'],
+    'exclude'   => true,
+    'inputType' => 'multiColumnWizard',
+    'sql'       => 'blob NULL',
+    'eval'      => array
     (
         'tl_class'     => 'clx',
         'columnFields' => array
         (
             'field_attribute' => array
             (
-                'label'                 => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute'],
-                'exclude'               => true,
-                'inputType'             => 'select',
-                'reference'             => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values'],
-                'eval' => array
-                    (
-                        'style'         => 'width:600px',
-                        'chosen'        => 'true',
-                    )
+                'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute'],
+                'exclude'   => true,
+                'inputType' => 'select',
+                'reference' => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values'],
+                'eval'      => array
+                (
+                    'style'  => 'width:600px',
+                    'chosen' => 'true',
+                )
             ),
         ),
     ),
@@ -59,16 +61,18 @@ $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['combinedvalues_fields'] 
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['force_combinedvalues'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues'],
-    'exclude'                 => true,
-    'inputType'               => 'checkbox',
-    'eval'                    => array('tl_class' => 'w50'),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues'],
+    'exclude'   => true,
+    'inputType' => 'checkbox',
+    'sql'       => 'char(1) NOT NULL default \'\'',
+    'eval'      => array('tl_class' => 'w50'),
 );
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['combinedvalues_format'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format'],
-    'exclude'                 => true,
-    'inputType'               => 'text',
-    'eval'                    => array('mandatory' => true, 'tl_class' => 'long'),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'sql'       => 'text NULL',
+    'eval'      => array('mandatory' => true, 'tl_class' => 'long'),
 );
